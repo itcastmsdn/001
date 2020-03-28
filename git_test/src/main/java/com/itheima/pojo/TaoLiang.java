982e3de4373f816d8bc1f6234857f642557455ec
itcast <bos@itcast.cn>
2020-03-29 00:14:01 +0800

    111

---
+++ git_test/src/main/java/com/itheima/pojo/TaoLiang.java
@@ -0,0 +1,14 @@
+package com.itheima.pojo;
+
+public class TaoLiang {
+    boolean handsome;
+
+    public boolean isHandsome() {
+        return handsome;
+    }
+
+    private void setHandsome(boolean handsome) {
+        handsome = true;
+        this.handsome = handsome;
+    }
+}
